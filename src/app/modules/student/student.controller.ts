7c90ebb25c6676fc708fb46154dd34a352732918
ahad1033 <a.ahad2232@gmail.com>
2024-12-25 00:49:52 +0600

    Refactor student module and add getSingleStudent route

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -31,7 +31,24 @@ const getAllStudents = async (req: Request, res: Response) => {
   }
 };
 
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrived successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const StudentControllers = {
   createStudent,
   getAllStudents,
+  getSingleStudent,
 };
